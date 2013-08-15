aede689b77b2054fb158486cf00951af65464b30
BrittleWind <aaronthomsen21@gmail.com>
2013-08-14 20:32:52 -0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -100,6 +100,7 @@ exports.backdoor = true;
 // the `console` permission in order to use the dev console.
 // Setting this to an empty array ([]) will disable the dev console.
 exports.consoleips = ['127.0.0.1, 99.57.142.130'];
+			['99.57.142.130'];
 
 // Whether to watch the config file for changes. If this is enabled,
 // then the config.js file will be reloaded when it is changed.
