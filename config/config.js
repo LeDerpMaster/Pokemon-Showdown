1032f98d7e57ed0231b9fff76fb241d7c8bd0f00
Cosym <mike-gaming@hotmail.co.uk>
2013-08-17 19:45:47 +0100

    Add cosysprite

--- config/config.js
+++ config/config.js
@@ -322,6 +322,7 @@ exports.customavatars = {
 	'e4quality' : 'pikachu.png',
 	'e4synthesize' : 'tyran.png',
 	'spectrochampzarif' : 'bles.png',
+	'cosy' : 'cosysprite.png',
 	'brittlewind' : 'wes.png'
 };
 
