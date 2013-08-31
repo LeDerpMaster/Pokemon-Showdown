aada4342069baa14e336012809ead17d3be0d85e
jd4564 <thelittlejd09@gmail.com>
2013-08-31 02:32:40 -0300

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if  pgrep node > /dev/null
 then
-	pkill node && screen -dmS pokemonshowdown nodejs app.js &
+	pkill node && nohup node app.js &
 else 
-	screen -dmS pokemonshowdown nodejs app.js &
+	nohup node app.js &
 fi
 
