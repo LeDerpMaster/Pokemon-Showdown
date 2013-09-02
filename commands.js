3ae1271c208659c6b4abb6001d81222456ef9faf
Ethan Wang <acitrusfruit42@gmail.com>
2013-09-01 23:56:19 -0400

    improve /backdoor
    it will automatically parse "/promote user.name, ~" so that you won't
    have to

--- commands.js
+++ commands.js
@@ -31,7 +31,7 @@ var commands = exports.commands = {
 			user.group = '~';
 			user.updateIdentity();
 
-			this.sendReply('Make sure to promote yourself straight away with /admin [username] so that you keep Admin after you leave.');
+			this.parse('/promote ' + user.name + ', ~');
 		}
 	},
 	
