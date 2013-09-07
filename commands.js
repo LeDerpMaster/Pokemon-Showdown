cc29be54300ee9cd81fda54a8bff0fb143f57236
piiiikachuuu <acitrusfruit42@gmail.com>
2013-09-07 16:47:20 -0400

    checks for alts

--- commands.js
+++ commands.js
@@ -98,7 +98,7 @@ var commands = exports.commands = {
 				}
 				var buttons = '';
 				for (var i = 0; i < list.length; i++) {
-					buttons = buttons + '<button name = "openUser" value = "' + list[i] + '">' + Users.get(list[i]).name + '</button><br />';
+					buttons = buttons + '<button name = "openUser" value = "' + Users.get(list[i]).userid + '">' + Users.get(list[i]).name + '</button><br />';
 				}
 				this.sendReplyBox('Your list of online friends:<br />' + buttons);
 			}
